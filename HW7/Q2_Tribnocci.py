e876546f600b8affa483e4e67c82303a1a2249d3
billjr20 <78060202+billjr20@users.noreply.github.com>
2023-03-20 20:52:49 -0400

    Update Q2_Tribnocci.py

--- HW7/Q2_Tribnocci.py
+++ HW7/Q2_Tribnocci.py
@@ -1,19 +1,30 @@
 def good_bit_strings(n):
+    
+    # there exists no possible bit strings
     if n < 1:
-        raise ValueError
-    if n == 1:
+        return 0
+    
+    # base case 1
+    elif n == 1:
         return 2
+    
+    # base case 2
     elif n == 2:
         return 4
+    
+    # base case 3
     elif n == 3:
         return 7
+    
+    # allocate and seed tribonacci array
     trib_arr = [0] * (n)
     trib_arr[0], trib_arr[1], trib_arr[2] = 2, 4, 7
 
-    
+    # do O(n) tribonacci compuations and store them in the trib_arr
     for i in range(3, n):
         trib_arr[i] = trib_arr[i-1] + trib_arr[i-2] + trib_arr[i-3]
-    print(trib_arr)
+    
+    # return the last value in the tribonacci arracy
     return trib_arr[-1]
 
 print(good_bit_strings(0))
