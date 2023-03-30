a57616a0973738bb68aabbe0600836b18ad351d4
billjr20 <78060202+billjr20@users.noreply.github.com>
2023-03-30 17:34:17 -0400

    Update Q2_and_3_knap_idea.py

--- HW8/Q2_and_3_knap_idea.py
+++ HW8/Q2_and_3_knap_idea.py
@@ -1,12 +1,11 @@
 import sys
-import collections
 
-def knapsack(weight, items):
+def knapsack(weight, items, left, right):
     dp_mat = [0] * (weight + 1)
-    K = [i for i in range(weight + 1)]    
+    K = [i for i in range(weight + 1)]
 
     # fill in the array in a bottom-up manner
-    for i in range(len(items)):
+    for i in range(left, right):
         item_w, item_v = items[i]
         # iterate over the possible weight limits in reverse order
         for j in range(weight, item_w - 1, -1):
@@ -14,29 +13,29 @@ def knapsack(weight, items):
             # against the value you could get by including the item
             without_item = dp_mat[j]
             with_item = dp_mat[j - item_w] + item_v
-            # dp_mat[j] = max(dp_mat[j], dp_mat[j - item_w] + item_v)
             if with_item > without_item:
                 dp_mat[j] = with_item
-                if i > (len(items)//2)-1:
+                if i > (right+left)//2-1:
                     K[j] = K[j - item_w]
-
+    
     return K[-1]
 
 
-def knapsack_idea(weight, items ):
-    if len(items) == 1:
-        return items if items[0][0] == weight else None
-    k = knapsack(weight, items)
-    print(k)
-    items_L, items_R = items[:len(items)//2], items[len(items)//2:] 
-    S_L = knapsack_idea(k, items_L)
-    S_R = knapsack_idea(weight-k, items_R)
+def knapsack_idea(weight, items, left, right):
+    print(weight, items)
+    if right - left == 1:
+        return [items[left]] if items[left][0] == weight else None
+    k = knapsack(weight, items, left, right)
+    mid = (left + right) // 2
+    S_L = knapsack_idea(k, items, left, mid)
+    S_R = knapsack_idea(weight-k, items, mid, right)
     if S_L and S_R:
         return S_L + S_R
     elif S_L:
         return S_L
     elif S_R:
         return S_R
+
 
 # All file parsing pieces are constant time operations as the 
 # size of a given line of input is fixed, so reading N items 
@@ -77,7 +76,7 @@ def main():
         return
 
     weight, items = openFile(sys.argv[1])
-    out_items = knapsack_idea(weight, items)
+    out_items = knapsack_idea(weight, items, 0, len(items))
     print(out_items)
 
 main()
